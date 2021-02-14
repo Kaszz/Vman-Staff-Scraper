6fcbc3cede62775bd2f744c99d51b30d077a3f6a
Kaszz <christopher@kas.net>
2021-02-14 13:52:54 +0100

    Add user input to specify staff options and open search page based on those params

--- scraper.py
+++ scraper.py
@@ -1,21 +1,14 @@
 from selenium import webdriver
 
-LOGIN_URL = 'https://www.virtualmanager.com/da/login'
-URL = 'https://www.virtualmanager.com/employees/search?utf8=%E2%9C%93&speciality=&country_id=&job_status=&age_min=&age_max=&search=1&commit=S%C3%B8g'
 
-#Function for logging in
-def login(credentials):
-    browser = webdriver.Chrome()
-    browser.get ('https://www.virtualmanager.com/da/login')
-    browser.find_element_by_id('email').send_keys(credentials['email'])
-    browser.find_element_by_id ('password').send_keys(credentials['password'])
-    browser.find_element_by_xpath('//*[@id="content"]/div/div/form/p[4]/input').click()
-    while(True):
-       pass
+def init_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('log-level=3')
+    driver = webdriver.Chrome(options=options)
+    return driver
 
-
-#Function for setting email and password
-def set_credentials():
+#Function for getting email and password
+def get_credentials():
     email = input("Enter email: ")
     password = input("Enter password: ")
 
@@ -23,11 +16,44 @@ def set_credentials():
         'email': email,
         'password': password
     }
+    return result
 
+#Function for logging in
+def login(driver, credentials):
+    driver.get('https://www.virtualmanager.com/da/login')
+    driver.find_element_by_id('email').send_keys(credentials['email'])
+    driver.find_element_by_id ('password').send_keys(credentials['password'])
+    driver.find_element_by_xpath('//*[@id="content"]/div/div/form/p[4]/input').click()
+
+
+#Function for getting trainer parameters
+def get_staff_params():
+    speciality = input("Speciality [coach, scout]: ")
+    job_status = input("Jobstatus | Unemployed [1] | Contracted [2]: ")
+    max_age = input("Max age: ")
+
+    result = {
+        'speciality': speciality,
+        'job_status': job_status,
+        'max_age': max_age
+    }
     return result
 
 
+def scrape(driver, params):
+    driver.get('https://www.virtualmanager.com/employees/search?utf8=%E2%9C%93&speciality=' + params['speciality'] + '&country_id=&job_status=' + params['job_status'] + '&age_min=&age_max=' + params['max_age'] + '&search=1&commit=S%C3%B8g')
+    
+
+
 if __name__ == '__main__':
-    credentials = set_credentials()
-    login(credentials)
+    driver = init_driver()
+
+    credentials = get_credentials()
+    login(driver, credentials)
+
+    params = get_staff_params()
+    scrape(driver, params)
+
+    while(True):
+        pass
 
